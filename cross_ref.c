1ff98113ed8b0b9a0c19905e2e83fba30442c030
Mark Jannenga <jannengm@mail.gvsu.edu>
2016-10-06 16:45:53 -0400

    Started linked list definition

--- cross_ref.c
+++ cross_ref.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+struct data{
+  char* identifier;
+  int line;   //Use _LINE_
+};
+
+struct node{
+  struct data d;
+  struct node *next;
+};
+
+struct list{
+  struct node* head;
+  struct node* tail;
+};
+
+typedef struct node node;
+typedef struct data data;
+typedef struct list list;
+
+void add(list* link_list, node* n );
+
 int main(int argc, char* argv[]){
   return 0;
+}
+
+void add(list* link_list, node* n ){
+  link_list->tail->next = n;
+  link_list->tail = n;
 }
