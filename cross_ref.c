960856c1438276be4c97c954f5a7bb4525e26e80
Mark Jannenga <jannengm@mail.gvsu.edu>
2016-10-08 18:34:33 -0400

    Added search function to linked list

--- cross_ref.c
+++ cross_ref.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 struct data{
   char* identifier;
@@ -20,12 +21,27 @@ typedef struct data data;
 typedef struct list list;
 
 void add(list* link_list, node* n );
+node* find_identifier(list* link_list, char* ident);
 
 int main(int argc, char* argv[]){
   return 0;
 }
 
+/*Add a node to the end of the linked list*/
 void add(list* link_list, node* n ){
   link_list->tail->next = n;
   link_list->tail = n;
+}
+
+/*Searches through the linked list for an identidier, returns
+ *a pointer to the node if found, else NULL*/
+node* find_identifier(list* link_list, char* ident){
+  node* n = link_list->head;
+  while( n != NULL ){
+    if( strcmp(n->d.identifier, ident) == 0 ){
+      break;
+    }
+    n++;
+  }
+  return n;
 }
